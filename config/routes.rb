37e585dff1c84780d487371c5e1c49ce1bcbebcd
MinamiSugiura <minami.sugiura29@gmail.com>
2018-04-20 16:40:44 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 SampleApp::Application.routes.draw do
     
-  get 'users/show'
-
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+  
   resources :users, only: [:show] #章7ユーザ情報を表示する
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  
-  resources :users, only: [:show]
+
 end 
